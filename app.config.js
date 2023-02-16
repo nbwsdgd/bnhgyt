ef1548a30140323a89bf8876c4bded4e724a541a
Wendy J Peterson <125609292+nbwsdgd@users.noreply.github.com>
2023-02-17 05:15:57 +0800

    Add files via upload

---
+++ app.config.js
@@ -0,0 +1,15 @@
+export default {
+  pages: [
+    'pages/hotSpot/hotSpotIndex',
+    'pages/diagnosis/diagnosis',
+    'pages/diagnosis/diagnosisStep3',
+    'pages/diagnosis/diagnosisStep2',
+    'pages/index/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+}
